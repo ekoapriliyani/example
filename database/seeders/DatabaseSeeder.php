7875fc6088e3528e717f59ab8bb03ebeb8e79271
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2026-06-03 07:55:02 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             // MaterialSeeder::class,
             UserSeeder::class,
-            MesinSeeder::class,
+            // MesinSeeder::class,
             SupplierSeeder::class,
             // ProductWmSeeder::class,
         ]);
